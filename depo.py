6ecfd4185f9b3cdce596cfe5b0b4a88ff8829ea4
brainsandwich <alexandre.beaudet@protonmail.ch>
2017-07-31 14:39:13 +0200

    depo 1.1 -> 1.2
    * simplified getpack function
    * new makepack function, that builds packages using cmake
    * getpack is now called in threads, to download multiples packs simultaneously
    * removed recurse function (it wasn't working great)
    * new config.json params : 'download-path', 'build-path', 'output-path'
    * 'path' has been removed from config.json params
    * new config.json params : 'build' (true / false) in package definition
    * new command argument '-b' / '--build' : build dowloaded packages if 'build' is true for package and --build argument has been passed

--- depo.py
+++ depo.py
@@ -9,13 +9,16 @@
 # Made by brainsandwich
 # Free of use
 
-import json, os, sys, subprocess, shutil, argparse
+import json, os, sys, subprocess, shutil, argparse, threading
 
-# 
-def getpack(root, name, origin, branch, version, force):
-	target = version if len(version) != 0 else branch
-	remote_target = 'origin/' + target if len(version) == 0 else target
+# ------------------------------------------------------
+# Functions
+# ------------------------------------------------------
 
+#
+# Dowload a package
+#
+def getpack(root, name, origin, target, remote_target, force):
 	# init repo in root dir
 	if not os.path.exists(root):
 		os.makedirs(root)
@@ -26,10 +29,32 @@ def getpack(root, name, origin, branch, version, force):
 		subprocess.call(['git', 'reset', '--hard', remote_target], cwd=root, stdout=console_out, stderr=console_out)
 
 	print('* Package \'' + name + '/' + target + '\' fetched ')
-
 	return
 
+#
+# Build a package
+#
+def makepack(srcpath, buildpath, outpath, generator, parameters):
+	print('* Making package \'' + name + ' ...')
+	sys.stdout.flush()
+
+	# CMake configure command
+	configure_command = ['cmake', '-G' + generator, '-H' + srcpath, '-B' + buildpath, '-DCMAKE_INSTALL_PREFIX=' + outpath]
+	if parameters is not None:
+		for param in parameters:
+			configure_command.append('-D' + param)
+	subprocess.call(configure_command, stdout=console_out, stderr=console_out)
+
+	# CMake build command
+	subprocess.call(['cmake', '--build', buildpath, '--target', 'INSTALL', '--config', 'Release'], stdout=console_out, stderr=console_out)
+
+	print('* Package \'' + name + ' ready.')
+	sys.stdout.flush()
+	return
+
+#
 # Self update function
+#
 def update_self():
 	import stat
 	repo = 'depo'
@@ -55,18 +80,40 @@ def update_self():
 	sys.stdout.flush()
 	return
 
+#
+# Package getter thread
+#
+class PackageGetter (threading.Thread):
+	def __init__(self, root, name, origin, target, remote_target, force):
+		threading.Thread.__init__(self)
+		self.root = root
+		self.name = name
+		self.origin = origin
+		self.target = target
+		self.remote_target = remote_target
+		self.force = force
 
+	def run(self):
+		getpack(self.root, self.name, self.origin, self.target, self.remote_target, self.force)
+
+# ------------------------------------------------------
+# Script
+# ------------------------------------------------------
+
+print('depo 1.2')
+sys.stdout.flush()
+
+# Parse command-line arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('-v', '--verbose', help='verbose output', action='store_true')
-parser.add_argument('-r', '--recurse', help='fetches dependencies of dependencies', action='store_true')
 parser.add_argument('-u', '--update', help='update this script', action='store_true')
 parser.add_argument('-c', '--clear', help='clear dependencies folder before fetching', action='store_true')
 parser.add_argument('-f', '--force', help='forces redownload of packages', action='store_true')
+parser.add_argument('-b', '--build', help='build packages', action='store_true')
 parser.add_argument('-i', '--input', help='path to the configuration file, default is \'./deps.json\'')
-parser.add_argument('-o', '--output', help='path to the dependencies installation folder, default is \'./external\'')
 args = parser.parse_args()
-sys.stdout.flush()
 
+# Keep quiet if not verbose
 console_out = sys.stdout if args.verbose else open(os.devnull, 'w')
 
 # Update if required
@@ -74,53 +121,70 @@ if args.update:
 	update_self()
 	sys.exit()
 
-# init
+# Init some paths
+script_path = os.path.dirname(os.path.realpath(__file__))
 config_path = args.input if args.input else 'deps.json'
-root_path = os.path.dirname(config_path)
-
-print('> Depo 1.2')
-print('> Configuration file : ' + os.path.abspath(config_path))
+print('> Configuration file : ' + config_path)
 if not os.path.exists(config_path):
 	sys.exit('! No config found ! Aborting ...')
 
-# parse config
+# Parse JSON config
 config_file = open(config_path, 'r')
 config = json.loads(config_file.read())
 config_file.close()
 
-# rmdir before doing anything
-deps_path = os.path.join(root_path, args.output if args.output else 'external')
-if args.clear and os.path.exists(deps_path):
+# Get config parameters
+generator = config['generator'] if 'generator' in config else ''
+dowload_path = config['download-path'] if 'download-path' in config else 'external/sources'
+build_path = config['build-path'] if 'build-path' in config else 'external/build'
+output_path = config['output-path'] if 'output-path' in config else 'external/output'
+
+# Clear directories
+if args.clear and os.path.exists(dowload_path):
 	def set_rw(operation, name, exc):
 		os.chmod(name, stat.S_IWRITE)
 		operation(name)
-	shutil.rmtree(os.path.abspath(deps_path), onerror=set_rw)
+	shutil.rmtree(os.path.abspath(dowload_path), onerror=set_rw)
+	shutil.rmtree(os.path.abspath(build_path), onerror=set_rw)
+	shutil.rmtree(os.path.abspath(output_path), onerror=set_rw)
 
-# create dir
-print('> Installing dependencies in ' + os.path.abspath(deps_path))
-if not os.path.exists(deps_path):
-    os.makedirs(deps_path)
+# Create download dir
+print('> Dowloading dependencies in ' + dowload_path)
+if not os.path.exists(dowload_path):
+    os.makedirs(dowload_path)
 
-# clone packages (assume all git for now)
+package_getters = []
+
+# Clone packages (assume all git for now)
 for package in config['packages']:
 	sys.stdout.flush()
 	name = package['name']
 	origin = package['origin']
-	repo_dir = os.path.join(deps_path, name)
+	repo_dir = os.path.join(dowload_path, name)
 
-	# one can either provide a version or a branch
+	# A version or a branch name must be set
 	version = package['version'] if 'version' in package else ''
 	branch = package['branch'] if 'branch' in package else ''
 	target = version if len(version) != 0 else branch
 	remote_target = 'origin/' + target if len(version) == 0 else target
 
-	getpack(repo_dir, name, origin, branch, version, args.force)
+	th = PackageGetter(repo_dir, name, origin, target, remote_target, args.force)
+	th.start()
+	package_getters.append(th)
 
-	if args.recurse and os.path.exists(os.path.join(repo_dir, 'deps.json')):
-		cmd = ['python', __file__, '-i', repo_dir, '-o', os.path.join(repo_dir, args.output if args.output else 'external'), '-r']
-		if args.force: cmd.push('-f')
-		print('$$ Fetching subdeps of \'' + name + '\'')
+# Wait for dowload completion
+for th in package_getters:
+	th.join()
+
+# Build packages
+if args.build and len(generator) > 0:
+	for package in config['packages']:
 		sys.stdout.flush()
-		subprocess.call(cmd)
+		name = package['name']
+		repo_dir = os.path.join(dowload_path, name)
+		build_dir = os.path.join(build_path, name)
+		build_package = package['build'] if 'build' in package else False
+		if build_package:
+			makepack(repo_dir, build_dir, output_path, generator, package['build-options'] if 'build-options' in package else None)
 
 print('> Dependencies ready')
